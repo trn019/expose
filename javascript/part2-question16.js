9dc9a5cc1af015d4e6a66e0ba82e8eb7eb70b7e4
trn019 <trn019@ucsd.edu>
2025-04-29 18:45:07 -0700

    changes

--- javascript/part2-question16.js
+++ javascript/part2-question16.js
@@ -11,4 +11,4 @@ for (let car in statistics) {
     if (car[0] === 'r' || statistics[car] % 2 === 1){
         console.log(statistics[car]);
     }
-}
+} 
--- javascript/part2-question18.js
+++ javascript/part2-question18.js
@@ -1,5 +1,5 @@
 
-setInterval(() => {
+setInterval(() => { 
     let d = new Date();
     let time = d.toLocaleTimeString();
     console.log(time);
